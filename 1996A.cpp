41e9053da872dd6c420c607200df1e0004f14a5a
Anshu Kumar Singh <anshu.singh.dev.official@gmail.com>
2024-09-05 23:41:40 +0530

    05-Sep-2024

---
+++ 1996A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAX = 1e5 + 5;
+const int MOD = 1e9 + 7;
+
+int main()
+{
+    ios_base :: sync_with_stdio (false);
+    cin.tie (NULL);
+    cout.tie (NULL);
+
+    int t, n;
+    cin >> t;
+
+    while (t --)
+    {
+        cin >> n;
+
+        cout << ((n / 4) + ((n % 4) >> 1)) << "\n";
+    }
+
+    return 0;
+}
